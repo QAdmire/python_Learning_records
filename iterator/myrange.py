e5767f01e44fdb1197c713d21f0bd6ec5fbd889e
Qyii <949579724@qq.com>
2021-04-15 15:50:40 +0800

    迭代器的应用

---
+++ iterator/myrange.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+class Myrange:
+    def __init__(self, n):
+        self.i = 1
+        self.n = n
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.i <= self.n:
+            i = self.i
+            self.i += 1
+            return i
+        else:
+            raise StopIteration()
+
+print("1",[i for i in Myrange(5)])
